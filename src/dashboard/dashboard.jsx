76c47363d58969dad705bec02facf0b8db64707f
Thomas Silva <thomas@allto.digital>
2025-06-09 14:07:13 -0300

    Criação de Reducers; Criação de componentes ValueBox e Row

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,16 +1,47 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
+import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
 
 class Dashboard extends Component {
   render() {
+    const { credit, debit } = this.props.summmary;
     return (
       <div>
         <ContentHeader title="Dashboard" subtitle="Versão 1.0" />
-        <Content>Dashboard</Content>
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value={`R$ ${credit}`}
+              text="Total de Créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value={`R$ ${debit}`}
+              text="Total de Débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value={`R$ ${credit - debit}`}
+              text="Valor Consolidado"
+            />
+          </Row>
+        </Content>
       </div>
     );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => ({ summary: state.dashboard.summary });
+
+export default connect(mapStateToProps)(Dashboard);
