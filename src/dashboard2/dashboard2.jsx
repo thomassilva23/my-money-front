06c2f0d9fc6d89232640036281c1a04e7d49a2b5
Thomas Silva <thomassilva23@hotmail.com>
2025-09-16 11:15:52 -0300

    feat: componentes de abas; ciclo de pagamentos; Reducers; Ações

---
+++ src/dashboard2/dashboard2.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
+
+const BASE_URL = "http://localhosT:3003/api";
+
+export default class Dashboard2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { credit: 0, debit: 0 };
+  }
+
+  componentWillMount() {
+    axios
+      .get(`${BASE_URL}/billingCycles/summary`)
+      .then((resp) => this.setState(resp.data));
+  }
+
+  render() {
+    const { credit, debit } = this.state;
+    return (
+      <div>
+        <ContentHeader title="Dashboard" subtitle="Versão 2.0" />
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value={`R$ ${credit}`}
+              text="Total de Créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value={`R$ ${debit}`}
+              text="Total de Débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value={`R$ ${credit - debit}`}
+              text="Valor Consolidado"
+            />
+          </Row>
+        </Content>
+      </div>
+    );
+  }
+}
