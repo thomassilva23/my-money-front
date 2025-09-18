dd48cdda758501457735a89e237ad7ecdbb1e01e
Thomas Silva <thomassilva23@hotmail.com>
2025-09-18 11:48:48 -0300

    feat: Componente BillingCycleList e integração com Redux

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -10,6 +10,7 @@ import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
 import { selectTab, showTabs } from "../common/tab/tabActions";
+import List from ",/BillingCycleList";
 
 class BillingCycle extends Component {
   componentWillMount() {
@@ -34,7 +35,7 @@ class BillingCycle extends Component {
             </TabsHeader>
             <TabsContent>
               <TabContent id="tabList">
-                <h1>Lista</h1>
+                <List />
               </TabContent>
               <TabContent id="tabCreate">
                 <h1>Incluir</h1>
