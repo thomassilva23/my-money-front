b830c120f0d097a3e6677c182ba22804c84a7e1c
Thomas Silva <thomas@allto.digital>
2025-09-20 23:11:50 -0300

    feat: Exibir aba alterar; inicializar formulário com dados

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { getList } from "./billingCycleActions";
+import { getList, showUpdate } from "./billingCycleActions";
 
 class BillingCycleList extends Component {
   componentWillMount() {
@@ -15,6 +15,14 @@ class BillingCycleList extends Component {
         <td>{bc.name}</td>
         <td>{bc.month}</td>
         <td>{bc.year}</td>
+        <td>
+          <button
+            className="btn btn-warning"
+            onClick={() => this.props.showUpdate(bc)}
+          >
+            <i className="fa fa-pencil"></i>
+          </button>
+        </td>
       </tr>
     ));
   }
@@ -28,6 +36,7 @@ class BillingCycleList extends Component {
               <th>Nome</th>
               <th>Mês</th>
               <th>Ano</th>
+              <th>Ações</th>
             </tr>
           </thead>
           <tbody>{this.renderRows()}</tbody>
@@ -39,5 +48,5 @@ class BillingCycleList extends Component {
 
 const mapStateToProps = (state) => ({ list: state.billingCycle.list });
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ getList }, dispatch);
+  bindActionCreators({ getList, showUpdate }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
