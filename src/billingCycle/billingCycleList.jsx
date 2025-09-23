5a8abc0abf72038db2b586e220cc140e153924d6
Thomas Silva <thomas@allto.digital>
2025-09-22 21:51:59 -0300

    feat: Excluir Ciclo de pagamento

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { getList, showUpdate } from "./billingCycleActions";
+import { getList, showUpdate, showDelete } from "./billingCycleActions";
 
 class BillingCycleList extends Component {
   componentWillMount() {
@@ -21,6 +21,12 @@ class BillingCycleList extends Component {
             onClick={() => this.props.showUpdate(bc)}
           >
             <i className="fa fa-pencil"></i>
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.props.showDelete(bc)}
+          >
+            <i className="fa fa-trash-o"></i>
           </button>
         </td>
       </tr>
@@ -48,5 +54,5 @@ class BillingCycleList extends Component {
 
 const mapStateToProps = (state) => ({ list: state.billingCycle.list });
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ getList, showUpdate }, dispatch);
+  bindActionCreators({ getList, showUpdate, showDelete }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
