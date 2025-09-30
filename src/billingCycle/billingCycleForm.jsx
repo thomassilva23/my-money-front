ce98f76a027c33da75f159d41cb3c15052b549a1
Thomas Silva <thomas@allto.digital>
2025-09-30 15:43:29 -0300

    feat: Componente Summary

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -6,6 +6,7 @@ import { reduxForm, Field, formValueSelector } from "redux-form";
 import { init } from "./billingCycleActions";
 import LabelAndInput from "../common/form/labelAndInput";
 import ItemList from "./itemList";
+import Summary from "./summary";
 
 class BillingCycleForm extends Component {
   render() {
@@ -40,6 +41,7 @@ class BillingCycleForm extends Component {
             placeholder="Informe o ano"
             type="number"
           />
+          <Summary credit={1000} debit={100} />
           <ItemList
             cols="12 6"
             list={credits}
